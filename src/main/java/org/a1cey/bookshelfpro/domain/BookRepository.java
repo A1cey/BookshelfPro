bfe1cc45652c59971bf343d7d19a65d6a52c48bd
A1cey <mschelkle05@web.de>
2026-03-06 11:58:52 +0100

    Added BookRepository and MediaItemRepository

--- src/main/java/org/a1cey/bookshelfpro/domain/BookRepository.java
+++ src/main/java/org/a1cey/bookshelfpro/domain/BookRepository.java
@@ -1,4 +1,36 @@
 package org.a1cey.bookshelfpro.domain;
 
+import org.jmolecules.ddd.annotation.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface BookRepository {
+
+    Book save(Book book);
+
+    Optional<Book> findByID(ID id);
+
+    Optional<Book> findByISBN(ISBN isbn);
+
+    Optional<Book> findByTitleContaining(String titleFragment);
+
+    Optional<Book> findByAuthors(List<String> authors);
+
+    Optional<Book> findByPublishDate(LocalDate publishDate);
+
+    Optional<Book> findByPublisher(String publisher);
+
+    Optional<Book> findByPublishPlace(String publishPlace);
+
+    Optional<Book> findByPageCount(int pageCount);
+
+    List<Book> findAll();
+
+    void deleteByID(ID id);
+
+    void deleteByISBN(ISBN isbn);
+
 }
--- src/main/java/org/a1cey/bookshelfpro/domain/MediaItemRepository.java
+++ src/main/java/org/a1cey/bookshelfpro/domain/MediaItemRepository.java
@@ -1,4 +1,19 @@
 package org.a1cey.bookshelfpro.domain;
 
+import org.jmolecules.ddd.annotation.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface MediaItemRepository {
+
+    Optional<MediaItem> findByID(ID id);
+
+    List<MediaItem> findAll();
+
+    List<MediaItem> findByTitleContaining(String titleFragment);
+
+    List<MediaItem> findByLabels(List<Label> labels);
+
 }
