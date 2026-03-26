1411a7c4d37c0ff3654e373c6ff401d51fa290b1
A1cey <mschelkle05@web.de>
2026-03-26 15:20:56 +0100

    Added UserID owner to BookshelfEntry

--- bookshelf-pro-domain/src/main/java/org/a1cey/bookshelf_pro_domain/bookshelf_entry/BookshelfEntry.java
+++ bookshelf-pro-domain/src/main/java/org/a1cey/bookshelf_pro_domain/bookshelf_entry/BookshelfEntry.java
@@ -3,9 +3,16 @@ package org.a1cey.bookshelf_pro_domain.bookshelf_entry;
 import jakarta.validation.Valid;
 import org.a1cey.bookshelf_pro_domain.bookshelf_entry.consumption.ConsumptionProgress;
 import org.a1cey.bookshelf_pro_domain.media_item.MediaItemID;
+import org.a1cey.bookshelf_pro_domain.user.UserID;
 import org.jmolecules.ddd.annotation.AggregateRoot;
 import org.jmolecules.ddd.annotation.Identity;
 
+//TODO: Is this really an aggregate root?
 @AggregateRoot
-public record BookshelfEntry(@Identity BookshelfEntryID id, MediaItemID mediaItemID, @Valid ConsumptionProgress consumptionProgress) {
+public record BookshelfEntry(
+        @Identity BookshelfEntryID id,
+        MediaItemID mediaItemID,
+        @Valid ConsumptionProgress consumptionProgress,
+        UserID owner
+) {
 }
